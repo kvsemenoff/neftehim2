136f8c831a04ff6d995198283a862a29a2e6ca23
boka <web_masters_07@mail.ru>
2017-05-28 15:54:35 +0300

    wp-1

--- wp-content/themes/neftehim/index.php
+++ wp-content/themes/neftehim/index.php
@@ -3,7 +3,42 @@
 <?php get_header(); ?>	
 	<div class="n-content">
 		<div class="n-content-inner vertical-align">	
-			<?php require_once('includes/product.php'); ?>
+			<div class="container">
+	
+		<div class="product-section">
+			<div class="product-section__box">
+				<div class="product-section__img">
+					<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/product1.jpg" alt=""></a>
+				</div>
+				<a href="#" class="button-more button-more_width">
+					<span class="button-more__bg">
+						<span class="button-more__txt">ПАРАФИНО-ВОСКОВАЯ ПРОДУКЦИЯ </span>
+					</span>
+				</a>
+			</div>
+			<div class="product-section__box">
+				<div class="product-section__img">
+					<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/product2.jpg" alt=""></a>
+				</div>				
+				<a href="#" class="button-more button-more_width">
+					<span class="button-more__bg">
+						<span class="button-more__txt">БИТУМЫ И МАЗУТЫ</span>
+					</span>
+				</a>
+			</div>
+			<div class="product-section__box product-section__box_last">
+				<div class="product-section__img">
+					<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/product3.jpg" alt=""></a>
+				</div>				
+				<a href="#" class="button-more button-more_width">
+					<span class="button-more__bg">
+						<span class="button-more__txt">НЕФТЕХИМИЯ</span>
+					</span>
+				</a>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+</div>
 			<?php require_once('includes/about.php'); ?>
 			<?php require_once('includes/news.php'); ?>
 		</div>
