10d4ae56f5afabc98cb0f90e65135e27457dce21
engine77 <zaur.1kager@gmail.com>
2017-05-28 19:14:19 +0400

    all file

--- wp-content/themes/neftehim/category-3.php
+++ wp-content/themes/neftehim/category-3.php
@@ -19,20 +19,50 @@
 				$query->the_post();
 			?>
 
-			<div class="news-item">
-				<a href="<?php the_permalink(); ?>" class="news-item__caption"><?php the_title(); ?></a>
-				<div class="news-item__img">
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+			
+
+
+			<div class="product-item">
+				<h4 class="product-item__caption"><?php the_title(); ?></h4>	
+				<div class="product-item__txtbox">	
+					<div class="product-item__img">
+
+						<?php if( class_exists('Dynamic_Featured_Image') ) { ?>
+						<?php global $dynamic_featured_image; ?>
+						<?php $featured_images = $dynamic_featured_image->get_featured_images( get_the_ID() ); ?>
+						<?php foreach( $featured_images as $image ) { ?>				
+
+						<a href="<?php echo $image['full'] ?>" data-fancybox-group="group">		<img src="<?php echo $image['full'] ?>" alt="">
+						</a>
+
+						<?php } ?>
+
+						<?php } ?>
+						
+					</div>		
+					<p><?php  do_excerpt(get_the_excerpt(), 15); ?>						
+					</p>	
 				</div>
-				<a href="<?php the_permalink(); ?>" class="news-item__txt"><?php  do_excerpt(get_the_excerpt(), 15); ?></a>
-				<span class="news-item__date"><?php the_time('d.m.Y'); ?></span>
-			</div>
-
-
-					
+				<div class="product-item__buttons">
+					<span class="product-item__pasport">Паспорта и сертификаты</span>
+					<div class="product-item__boxbuttons">
+						<a href="<?php the_permalink(); ?>" class="product-item__look">Посмотреть</a>
+						<a href="<?php the_permalink(); ?>" class="product-item__download">Скачать</a>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>		
 			
 			<?php }?>
 			
+
+			<div class="news-button">
+				<a href="#" class="button-more button-more_width">
+					<span class="button-more__bg">
+						<span class="button-more__txt">ПЕРЕЙТИ К ПОЛНОМУ КАТАЛОГУ ПРОДУКЦИИ</span>
+					</span>
+				</a>
+			</div>
 		</div>
 	</div>
 </div>
