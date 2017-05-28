c6782836e9009ea91d8353643d297553b824b9bb
engine77 <zaur.1kager@gmail.com>
2017-05-28 18:02:36 +0400

    all file

--- wp-content/themes/neftehim/page.php
+++ wp-content/themes/neftehim/page.php
@@ -2,27 +2,26 @@
 
 
 
-
-<div class="container newsbg">				
-	<h1 class="h2 about__caption"><?php the_title(); ?></h1>
-	<div class="news">	
-
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-			<div class="news-item">
-				<a href="<?php the_permalink(); ?>" class="news-item__caption"><?php the_title(); ?></a>
-				<div class="news-item__img">
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+<div class="news-page">
+	<div class="container">				
+		<div class="box">
+			<h1 class="h2"><?php the_title(); ?></h1>
+			<div class="content clearfix">						
+				<div class="img-wrap">
+					<img src="img/news-item4.jpg" alt="">
 				</div>
-				<a href="<?php the_permalink(); ?>" class="news-item__txt"><?php  do_excerpt(get_the_excerpt(), 15); ?></a>
-				<span class="news-item__date"><?php the_time('d.m.Y'); ?></span>
-			</div>	
 
-	<?php endwhile; ?>
-	<?php else: ?>
-	<?php endif; ?> 	
-</div>
-</div>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+					<p class="news-page__p_move"><?php the_content(); ?> </p>
+
+
+				<?php endwhile; ?>
+				<?php else: ?>
+				<?php endif; ?> 
+			</div>	
+		</div>
+	</div>
 </div>
 
 
