873baedd36fc65f8e7aca401fdc48ec794a3aa37
boka <web_masters_07@mail.ru>
2017-05-28 15:01:41 +0300

    wp-1

--- wp-content/themes/neftehim/index.php
+++ wp-content/themes/neftehim/index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Главная</title>
-<?php require_once('includes/header.php'); ?>
+<?php get_header(); ?>
 <div class="n-wrapper">
 	<div class="n-header">
 		<?php require_once('includes/header-top.php'); ?>
