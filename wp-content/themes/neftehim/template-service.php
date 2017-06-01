9057d272c1f7ce913fcf8a0b9ebab050c3a6a31d
boka <web_masters_07@mail.ru>
2017-06-01 18:24:41 +0300

    com

--- wp-content/themes/neftehim/template-service.php
+++ wp-content/themes/neftehim/template-service.php
@@ -22,13 +22,35 @@ Template name: Направления
 							<h1 class="h2"><?php the_title(); ?></h1>
 							<?php the_content(); ?>
 						</div>
-						<?php }?>	
+						<?php }?>
+						<div class="box">
+							<h1 class="h2">Документы</h1>
+							<div class="row">
+								<div class="docs clearfix">
+									<span class="box-descr">ОБРАЗЕЦ ДОГОВОРА</span>
+									<?php 
+									$args = array(
+										'cat' => 8,
+										);	
+									$query = new WP_Query($args);
+									while ( $query->have_posts() ) {
+										$query->the_post();
+										?>
+										<div class="doc-wrap">
+											<div class="doc-item">
+												<a class="doc" data-fancybox-group=group	href="../../img/doc.jpg"><img src="../../img/doc.jpg" alt=""></a><br/>
+												<a class="doc" data-fancybox-group=group	href="../../img/doc.jpg">Посмотреть</a> <a href="#">Скачать</a>
+											</div>
+										</div>
+										<?php }?>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
-<?php get_footer(); ?>	
+		<?php get_footer(); ?>	
 
 
