7b7db59e7087893668c1c3fda9d84cf8ba068ef1
boka <web_masters_07@mail.ru>
2017-06-01 16:16:24 +0300

    com

--- wp-content/themes/neftehim/template-service.php
+++ wp-content/themes/neftehim/template-service.php
@@ -6,17 +6,29 @@ Template name: Направления
 <?php get_header(); ?>
 <div class="n-content">
 	<div class="n-content-inner vertical-align">
-		<?php 
-		$args = array(
-			'cat' => 7,
-			);
+		<div class="service-page">
+			<div class="container">
+				<div class="row">
+					<?php 
+					$args = array(
+						'cat' => 7,
+						);
 
-		$query = new WP_Query($args);
-		while ( $query->have_posts() ) {
-			$query->the_post();
-			?>
-			
-			<?php }?>	
+					$query = new WP_Query($args);
+					while ( $query->have_posts() ) {
+						$query->the_post();
+						?>
+						<div class="box">
+							<h1 class="h2"><?php the_title(); ?></h1>
+							<?php the_content(); ?>
+						</div>
+						<?php }?>	
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-	<?php get_footer(); ?>	
+</div>
+<?php get_footer(); ?>	
+
+
