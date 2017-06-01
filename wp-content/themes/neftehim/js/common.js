4fab700fe363c0acb0dfd02cc1aa54af507b53ab
engine77 <zaur.1kager@gmail.com>
2017-06-01 18:56:57 +0400

    all file

--- wp-content/themes/neftehim/js/common.js
+++ wp-content/themes/neftehim/js/common.js
@@ -98,6 +98,18 @@ $(document).ready(function(){
 			scrollTop: scrollTopH
 		}, 500);
 	});
+
+
+
+
+	$("a[data-fancybox-group=group]").fancybox({
+		'transitionIn' : 'none',
+		'transitionOut' : 'none',
+		'titlePosition' : 'over',
+		'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+		}
+	});
 });//END READY
 
 
