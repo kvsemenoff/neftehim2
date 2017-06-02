f3583407906fb7ad81d6db44109db544e5ad9e2d
Berzeg07 <berezg86@gmail.com>
2017-06-02 12:24:21 +0400

    31.05

---
+++ wp-content/themes/neftehim/searchform.php
@@ -0,0 +1,7 @@
+
+<div class="footer_form">
+	<form action="<?php echo home_url(); ?>" method="get" class="search-box">
+		<input type="text"  name="s">
+		<input type="submit" value="">
+	</form>
+</div>	
