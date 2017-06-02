d6dc57edf5986c1fdc555a23529224d8627e171c
Berzeg07 <berezg86@gmail.com>
2017-06-02 11:49:56 +0400

    31.05

--- wp-content/themes/neftehim/search.php
+++ wp-content/themes/neftehim/search.php
@@ -1,12 +1,12 @@
 
 <?php get_header(); ?>	
-
+<br/>
 <h1>Поиск по: "<?php echo $_GET['s'];?>"</h1><br/><br/>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<a href="<?php the_permalink();?>"><?php the_title(); ?></a><br/><br/>
 
 <?php endwhile; else: ?>
-	<p>Поиск не дал результатов.</p>
+	<p>Поиск не дал результатов.</p><br/><br/>
 <?php endif;?>
 
 <?php get_footer(); ?>	
