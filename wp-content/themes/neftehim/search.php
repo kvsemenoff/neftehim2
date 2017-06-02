1eed67a0e403b7a3acb828f60e1a2f8fe3967644
Berzeg07 <berezg86@gmail.com>
2017-06-02 12:11:31 +0400

    31.05

--- wp-content/themes/neftehim/search.php
+++ wp-content/themes/neftehim/search.php
@@ -1,12 +1,26 @@
 
 <?php get_header(); ?>	
-<br/>
-<h1>Поиск по: "<?php echo $_GET['s'];?>"</h1><br/><br/>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<a href="<?php the_permalink();?>"><?php the_title(); ?></a><br/><br/>
 
-<?php endwhile; else: ?>
-	<p>Поиск не дал результатов.</p><br/><br/>
-<?php endif;?>
+
+<div class="news-page">
+	<div class="container">				
+		<div class="box">
+			<h1 class="h2"><?php the_title(); ?></h1>
+			<div class="content clearfix">						
+				
+
+				<br/>
+				<h1>Поиск по: "<?php echo $_GET['s'];?>"</h1><br/><br/>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<a href="<?php the_permalink();?>"><?php the_title(); ?></a><br/><br/>
+
+				<?php endwhile; else: ?>
+				<p>Поиск не дал результатов.</p><br/><br/>
+			<?php endif;?>
+			
+		</div>	
+	</div>
+</div>
+</div>
 
 <?php get_footer(); ?>	
