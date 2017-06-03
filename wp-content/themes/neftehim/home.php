a99ce68695e950e5e53b39fdbba29d5e4ea9fe31
boka <web_masters_07@mail.ru>
2017-06-03 12:18:26 +0300

    home

--- wp-content/themes/neftehim/home.php
+++ wp-content/themes/neftehim/home.php
@@ -44,18 +44,13 @@ Template Name: Главная страница
 		</div>
 		<div class="clearfix"></div>
 </div>
+
 			<div class="aboutbackground">
 	<div class="container">
 		<div class="aboutblock">		
 			<div class="about__txtbox">
 				<h1 class="h2 about__caption">О КОМПАНИИ</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam interdum quam vitae ligula fringilla, sit amet tincidunt est bibendum. Fusce consectetur ligula mauris, <a href="#">efficitur interdum erat dapibus id. </a> <a href="#"> In hac habitasse platea dictumst.</a> Nunc facilisis risus nisi, ac vulputate odio ullamcorper eget. Cras quis tortor at justo venenatis accumsan sit amet et tortor. Morbi scelerisque venenatis ex a volutpat. In placerat sapien eu fermentum tristique.</p>
-				<p>Phasellus tincidunt sed erat id vehicula. Vestibulum vitae libero metus. Vivamus sodales enim eu euismod sollicitudin. Quisque sagittis ullamcorper dui eget dapibus. Donec non nulla augue. Vestibulum pulvinar justo sit amet porttitor vehicula. Suspendisse potenti. Maecenas faucibus lorem a varius commodo.</p>
-				<p>Integer porta dictum ultrices. Curabitur et turpis nunc. Phasellus nec lorem et nunc tristique aliquet. Cras semper viverra tincidunt. Fusce ut sollicitudin ligula, ut scelerisque quam. Nulla leo felis, dapibus at ullamcorper quis, consectetur sed dui. Cras fermentum faucibus elit. Suspendisse ullamcorper vel massa vel bibendum. Fusce sed ultricies nisl. Curabitur vulputate metus ut condimentum condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in ipsum felis. Duis egestas in turpis eget finibus. Sed vitae nisl vitae nisi mattis imperdiet at non tellus. Nulla nec semper lorem. Integer malesuada euismod tempor.</p>
-				<p>Sed et rutrum augue, varius vehicula lorem. Donec facilisis augue semper felis interdum gravida. Quisque posuere mauris vitae nulla auctor consectetur. Nunc sollicitudin a lorem sit amet rhoncus. Sed tincidunt urna vehicula magna dictum malesuada et eleifend dolor. Fusce arcu odio, pharetra ut faucibus in, scelerisque id tortor. Praesent ac felis dui. Curabitur at dictum magna. Pellentesque fringilla dignissim ipsum</p>
-				<p>Phasellus tincidunt sed erat id vehicula. Vestibulum vitae libero metus. Vivamus sodales enim eu euismod sollicitudin. Quisque sagittis ullamcorper dui eget dapibus. Donec non nulla augue. Vestibulum pulvinar justo sit amet porttitor vehicula. Suspendisse potenti. Maecenas faucibus lorem a varius commodo.
-
-				 </p>
+				<p>	<?php the_field('о_компании'); ?></p>
 				
 			</div>
 			<div class="about__formbox">
