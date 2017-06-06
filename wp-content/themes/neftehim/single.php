ba686835078c089aa87c405c8246de26b3ef4ac4
engine77 <zaur.1kager@gmail.com>
2017-06-06 10:05:07 +0400

    first commit

--- wp-content/themes/neftehim/single.php
+++ wp-content/themes/neftehim/single.php
@@ -1,28 +1,30 @@
 <?php get_header(); ?>
 
 
+<div class="n-content">
+	<div class="n-content-inner vertical-align news-page">	
+	
+			<div class="container">				
+				<div class="box">
+					<h1 class="h2"><?php the_title(); ?></h1>
+					<div class="content clearfix">						
+						<div class="img-wrap">
+							<img src="img/news-item4.jpg" alt="">
+						</div>
 
-<div class="news-page">
-	<div class="container">				
-		<div class="box">
-			<h1 class="h2"><?php the_title(); ?></h1>
-			<div class="content clearfix">						
-				<div class="img-wrap">
-					<img src="img/news-item4.jpg" alt="">
-				</div>
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-					<?php the_content(); ?>
+							<?php the_content(); ?>
 
 
-				<?php endwhile; ?>
-				<?php else: ?>
-				<?php endif; ?> 
-			</div>	
+						<?php endwhile; ?>
+					<?php else: ?>
+					<?php endif; ?> 
+				</div>	
+			</div>
 		</div>
-	</div>
-</div>
 
+</div>
+</div>
 
 <?php get_footer(); ?>	
